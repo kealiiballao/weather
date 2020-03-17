eaafb740c2339debd0fff55433eac268b9592d9b
Kealii Ballao <ballao@hawaii.edu>
2020-03-17 12:46:31 -1000

    Updated the code to pull in long and lat

--- index.js
+++ index.js
@@ -13,52 +13,35 @@ app.get("/", function(req, res) {
 
 //invoked after hitting go in the html form
 app.post("/", function(req, res) {
+    let lat = String(req.body.latitude);
+    let lon = String(req.body.longitude);
 
-    // takes in the city from the html form, display in console. Takes in as String
-        const coordLong =parseFloat(req.body.longitude);
-        const coordLat = parseFloat(req.body.latitude);
-        console.log(coordLong +", "+coordLat);
-
-    //build up the URL for the JSON query, API Key is e2a23207324a3ca9e15f4b78edef5b69 secret and needs to be obtained by signup
         const units = "imperial";
         const apiKey = "e2a23207324a3ca9e15f4b78edef5b69";//secret key created on openweathermap
-        const url = "https://api.openweathermap.org/data/2.5/weather" + "?lat=" + coordLat +"&lon=" + coordLong +  "&units=" + units + "&APPID=" + apiKey; //url needed to query by city. See openweathermap city section.
-
+        const url = "https://api.openweathermap.org/data/2.5/weather"+"?lat="+lat+"&lon="+lon+"&units="+units+"&appid="+apiKey;
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
+        console.log(response.statusCode);
         // gets individual items from Open Weather API
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
-            const tempMin = weatherData.main.temp_min;
-            const tempMax = weatherData.main.temp_max;
+            const maxTemp = weatherData.main.temp_max;
+            const minTemp = weatherData.main.temp_min;
             const windSpeed = weatherData.wind.speed;
-            const windDirection = weatherData.wind.deg;
-            const feelsLike = weatherData.main.feels_like;
-           const city = weatherData.name;
-           let myWeatherTestArray = [city,tempMin,tempMax,windSpeed,windDirection,feelsLike];
-           for (let i = 0; i < myWeatherTestArray.length; i++) {
-               console.log(myWeatherTestArray[i]);
-               
-           };
-           
-           /*
-            
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            // displays the output of the results
-            res.write("<h1> The weather is " + weatherDescription + "</h1>");
-            res.write("<p>The Temperature in " + city +" is " + temp + "&#8457; </p>");//took out zip variable because not needed.
-            res.write("<div><img src=" + imageURL +">"+"Feels like: "+ feelsLike+"</div>");
-           
-           */
-          res.send(tempMin +",<br>"+ tempMax + ",<br>" + windSpeed +",<br>"+ windDirection + ",<br>" + feelsLike);//need to send data to webpage.
+            const windDirect = parseFloat(weatherData.wind.deg);
+            const city = weatherData.name;
+            res.write("<h1>Minimum Temperature is "+minTemp+"&#8457;</h1>");
+            res.write("<h1>Max Temperature is "+maxTemp+"&#8457;</h1>");
+            res.write("<h1>Wind Speed is "+windSpeed+" m/h</h1>");
+            res.write("<h1>Wind Direction is "+windDirect+"&deg;</h1>");
+            res.write("<h1> Nearest location is "+city+".</h1>");
+            res.send()
         });
     });
 })
 
 
 //Code will run on 3000 or any available open port
-app.listen(process.env.PORT || 3003, function() {
-console.log ("Server is running on port 3003")
+app.listen(process.env.PORT || 3000, function() {
+console.log ("Server is running on port 3000")
 });
